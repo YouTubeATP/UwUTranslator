a97cf73a3129c2555483922cca90c24fce95c36b
YouTubeATP <YouTubeATP@users.noreply.github.com>
2020-10-08 20:19:55 +0800

    Organized the code for a bit

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 void ReplaceStringInPlace(string& subject, const string& search,
                           const string& replace) {
     size_t pos = 0;
-    while ((pos = subject.find(search, pos)) != string::npos) {
+    while((pos = subject.find(search, pos)) != string::npos) {
          subject.replace(pos, search.length(), replace);
          pos += replace.length();
     }
@@ -14,8 +14,8 @@ void ReplaceStringInPlace(string& subject, const string& search,
 
 int main() {
     string s;
-    printf ("What do you need to translate today?\n");
-    getline (cin, s);
+    printf("What do you need to translate today?\n");
+    getline(cin, s);
     string q[] = {"Na", "na", "Ne", "ne", "Ni", "ni", "No", "no", "Nu", "nu", "R", "r", "L", "l", "Ove", "ove", "Th", "th", "!"};
     string r[] = {"Nya", "nya", "Nye", "nye", "Nyi", "nyi", "Nyo", "nyo", "Nyu", "nyu", "W", "w", "W", "w", "Uv", "uv", "D", "d", "! UwU"};
     for(int i = 0; i < 19; i++) {
